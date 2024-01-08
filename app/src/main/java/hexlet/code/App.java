6a143929922da6847683340a0454714ef651c3a8
ivan dyogtev <diogteff.ivan@yandex.ru>
2024-01-08 18:03:35 +0200

    create Makefile & update class App with using picocli utility

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,25 @@
 package hexlet.code;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
 
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello, world!");
+import java.io.File;
+import java.math.BigInteger;
+import java.nio.file.Files;
+import java.security.MessageDigest;
+import java.util.concurrent.Callable;
+@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "App 1.0",
+        description = "Compares two configuration files and shows a difference.")
+
+class App implements Runnable {
+    @Override
+    public void run() {
+        System.out.println("Hello, world");
+    }
+    public static void main(String... args) {
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 }
+
