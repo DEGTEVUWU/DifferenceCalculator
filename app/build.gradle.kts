b3958a5a07c33c22b4de0d537526f5dde23a3ec7
ivan dyogtev <diogteff.ivan@yandex.ru>
2024-01-10 12:36:43 +0200

    change main.yml and change gradlew

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("java")
     application
     checkstyle
+    jacoco
 }
 
 
@@ -24,3 +25,4 @@ dependencies {
 tasks.test {
     useJUnitPlatform()
 }
+tasks.jacocoTestReport { reports { xml.required.set(true) } }
