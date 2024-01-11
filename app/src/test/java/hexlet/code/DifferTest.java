1c40f36643898444bfc0b6e2efc45973539c72e3
ivan dyogtev <diogteff.ivan@yandex.ru>
2024-01-11 16:02:13 +0200

    add test for void json-files

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -14,10 +14,19 @@ class DifferTest {
         String value1 = "./src/test/resources/test1.json";
         String value2 = "./src/test/resources/test2.json";
 
-        //File file1 = new File("test1.json");
-        //File file2 = new File("test2.json");
         var actual = Differ.generate(value1, value2);
         var extended = "{ \n" + "    host=hexlet.io\n" + "  - verbose=true\n" + "}";
+        assertThat(actual).isEqualTo(extended);
+
+    }
+
+    @Test
+    void generateVoid() throws IOException {
+        String value1 = "./src/test/resources/voidFile1.json";
+        String value2 = "./src/test/resources/voidFile1.json";
+
+        var actual = Differ.generate(value1, value2);
+        var extended = "{ \n}";
         assertThat(actual).isEqualTo(extended);
 
     }
