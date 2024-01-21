741588999dc0173e8b7f977e413c47f4ce16144f
ivan dyogtev <diogteff.ivan@yandex.ru>
2024-01-21 19:19:14 +0200

    change logic in PropertyValue.class

--- app/src/main/java/hexlet/code/formatters/PropertyValue.java
+++ app/src/main/java/hexlet/code/formatters/PropertyValue.java
@@ -2,9 +2,11 @@ package hexlet.code.formatters;
 
 public class PropertyValue {
     public static String propertyValue(Object value1) {
-        if (value1 instanceof Integer || value1 instanceof Character
-                || value1 instanceof Boolean || value1 instanceof Double || value1 instanceof Float
-                || value1 instanceof Long || value1 == null) {
+        if (value1 instanceof Integer || value1 instanceof Character) {
+            return "Simple type";
+        } else if (value1 instanceof Boolean || value1 instanceof Double) {
+            return"Simple type";
+        } else if (value1 instanceof Float || value1 instanceof Long || value1 == null) {
             return "Simple type";
         } else if (value1 instanceof String) {
             return  "String type";
