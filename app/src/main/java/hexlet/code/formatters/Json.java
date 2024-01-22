325aa5c671a54294b33bafc7f816b8a023f5eff1
ivan dyogtev <diogteff.ivan@yandex.ru>
2024-01-22 12:24:15 +0200

    deleted not actuality code from Json.class

--- app/src/main/java/hexlet/code/formatters/Json.java
+++ app/src/main/java/hexlet/code/formatters/Json.java
@@ -7,73 +7,7 @@ import java.util.TreeMap;
 
 public class Json {
     public static String json(TreeMap<String, Status> mapWithKeysStatus) throws JsonProcessingException {
-        String resultString = "{";
-
         ObjectMapper mapper = new ObjectMapper();
         return mapper.writeValueAsString(mapWithKeysStatus);
-
-        /*
-        for (var keyWithStatus : mapWithKeysStatus.entrySet()) {
-            var key = keyWithStatus.getKey();
-            var data = keyWithStatus.getValue();
-
-            if (data.isStatus().equals("unchanged")) {
-                Object value;
-
-                if (PropertyValue.propertyValue(data.getOldValue()).equals("String type")) {
-                    value = "\"" + data.getOldValue() + "\"";
-                } else {
-                    value = data.getOldValue();
-                }
-
-                resultString += "\"" + key + "\"" + ":" + value + ",";
-
-            } else if (data.isStatus().equals("changed")) {
-                Object value1;
-                Object value2;
-
-                if (PropertyValue.propertyValue(data.getOldValue()).equals("String type")) {
-                    value1 = "\"" + data.getOldValue() + "\"";
-                } else {
-                    value1 = data.getOldValue();
-                }
-                if (PropertyValue.propertyValue(data.getNewValue()).equals("String type")) {
-                    value2 = "\"" + data.getNewValue() + "\"";
-                } else {
-                    value2 = data.getNewValue();
-                }
-
-                resultString += "\"" + "-" + key + "\"" + ":" + value1 + ",";
-                resultString += "\"" + "+" + key + "\"" + ":" + value2 + ",";
-
-            } else if (data.isStatus().equals("deleted")) {
-                Object value;
-
-                if (PropertyValue.propertyValue(data.getOldValue()).equals("String type")) {
-                    value = "\"" + data.getOldValue() + "\"";
-                } else {
-                    value = data.getOldValue();
-                }
-
-                resultString += "\"" + "-" + key + "\"" + ":" + value + ",";
-
-            } else if (data.isStatus().equals("added")) {
-                Object value;
-
-                if (PropertyValue.propertyValue(data.getNewValue()).equals("String type")) {
-                    value = "\"" + data.getNewValue() + "\"";
-                } else {
-                    value = data.getNewValue();
-                }
-
-                resultString += "\"" + "+" + key + "\"" + ":" + value + ",";
-            }
-        }
-
-        resultString = StringUtils.chop(resultString);
-        resultString += "}";
-        //return resultString;
-
-         */
     }
 }
