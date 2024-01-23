650ebccadccc2016286aa9122f9f8cbfc9390569
ivan dyogtev <diogteff.ivan@yandex.ru>
2024-01-23 16:36:51 +0200

    fix Formater.class and Differtest

--- app/src/main/java/hexlet/code/Formatter.java
+++ app/src/main/java/hexlet/code/Formatter.java
@@ -3,19 +3,30 @@ package hexlet.code;
 import hexlet.code.formatters.Json;
 import hexlet.code.formatters.Plain;
 import hexlet.code.formatters.Stylish;
+
 import java.io.IOException;
 import java.util.TreeMap;
 
 public class Formatter {
     public static String formatter(TreeMap<String, Status> mapWithStatusKeys, String format)
             throws IOException {
-        String resultString = "";
         return switch (format) {
-            case "stylish" -> resultString = Stylish.render(mapWithStatusKeys);
-            case "plain" -> resultString = Plain.render(mapWithStatusKeys);
-            case "json" -> resultString = Json.render(mapWithStatusKeys);
+            case "stylish" -> Stylish.render(mapWithStatusKeys);
+            case "plain" -> Plain.render(mapWithStatusKeys);
+            case "json" -> Json.render(mapWithStatusKeys);
             default -> throw new IOException("Unknown format: '" + format + "'");
         };
+
+        /*
+        if (format.equals("stylish")) {
+            resultString = Stylish.render(mapWithStatusKeys);
+        } else if (format.equals("plain")) {
+            resultString = Plain.render(mapWithStatusKeys);
+        } else if (format.equals("json")) {
+            resultString = Json.render(mapWithStatusKeys);
+        }
+
+         */
 
     }
 }
--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -1,10 +1,8 @@
 package hexlet.code;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
